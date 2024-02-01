e6b3e4731155d94fdbdb4a334718838ce9610163
Jenya <113167825+Genya10@users.noreply.github.com>
2024-02-01 16:11:54 +0200

    filling index.js

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import Fingerprint from "express-fingerprint";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+
+app.use(
+  Fingerprint({
+    parameters: [Fingerprint.useragent, Fingerprint.acceptHeaders],
+  })
+);
+
+app.listen(PORT, () => {
+    console.log("Сервер успешно запущен");
+  });
+  
