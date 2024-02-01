c4b46c8b22b178222c29c11c7111af9b6fd7dee5
Jenya <113167825+Genya10@users.noreply.github.com>
2024-02-01 16:33:06 +0200

    create scss files

---
+++ client/src/pages/Demo.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import style from "./style.module.scss";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Demo() {
+    const { data, handleLogOut, handleFetchProtected } = useContext(AuthContext);
+  
+    return (
+      <div className={style.wrapper}>
+        <p>{JSON.stringify(data)}</p>
+        <Button onClick={handleFetchProtected}>
+        Request for a protected route
+        </Button>
+        <Button onClick={handleLogOut}>
+        Go out
+        </Button>
+      </div>
+    );
+  }
