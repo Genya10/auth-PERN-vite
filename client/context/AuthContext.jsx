03c1da6bf75e9b96b47008e90db07a337cae4da7
Jenya <113167825+Genya10@users.noreply.github.com>
2024-02-01 16:17:34 +0200

    create AuthContext

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const AuthClient = axios.create({
+    baseURL:`${config.API_URL}/auth`
+  })
+  
+  export const AuthContext = createContext({});
+  
+  const AuthProvider = ({ children }) => {
+    const [data, setData] = useState();
+  
+    const handleFetchProtected = () => {};
+  
+    const handleLogOut = () => {};
+  
+    const handleSignUp = (data) => {};
+    
+  
+    const handleSignIn = (data) => {};
+  
+    return (
+      <AuthContext.Provider
+        value={{
+          data,
+          handleFetchProtected,
+          handleSignUp,
+          handleSignIn,
+          handleLogOut,
+        }}
+      >
+        {children}
+      </AuthContext.Provider>
+    );
+  };
+  
+  export default AuthProvider;
