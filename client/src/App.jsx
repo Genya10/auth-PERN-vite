b440a67d478a0647c9aabfe33cd786c6f8f72f61
Jenya <113167825+Genya10@users.noreply.github.com>
2024-02-02 09:13:01 +0200

    filling App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import style from "./app.module.scss";
+import { Link, Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Demo from "./pages/Demo";
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-   Vite
+const App =()=>{
+  return(
+    <div className={style.wrapper}>
+      <BrowserRouter>
+        <nav className={style.nav}>
+          <Link to="sign-in">Enter</Link>
+          <Link to="sign-up">Registration</Link>
+          <Link to="demo">Demo</Link>
+        </nav>
+        <Routes>
+          <Route path="demo" element={<Demo />} />
+          <Route path="sign-in" element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="*" element={<Navigate to={"sign-in"} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
